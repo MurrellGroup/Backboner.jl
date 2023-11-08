1a065573c7fad63c51815a0f95a41eaa3b852062
anton083 <anton.oresten42@gmail.com>
2023-11-08 23:02:42 +0100

    Switched to registered ASS version

--- src/assign.jl
+++ src/assign.jl
@@ -6,7 +6,7 @@ import AssigningSecondaryStructure as ASS
     assign_secondary_structure!(backbone)
 """
 function assign_secondary_structure!(backbone::Backbone{4})
-    ssvectors = ASS.dssp([chain.coords for chain in backbone])
+    ssvectors = ASS.assign_secondary_structure([chain.coords for chain in backbone])
     for (chain, ssvector) in zip(backbone, ssvectors)
         @assert length(chain.ssvector) == length(ssvector)
         chain.ssvector .= SecondaryStructure.(ssvector)
