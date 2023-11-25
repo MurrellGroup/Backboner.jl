9492e1fddeccd460dcf78698605a19b2c765637d
anton083 <anton.oresten42@gmail.com>
2023-11-25 13:59:13 +0100

    aaseq and OXT atom

--- src/io.jl
+++ src/io.jl
@@ -1,5 +1,13 @@
 export pdb_to_protein, protein_to_pdb
 
+const THREE_LETTER_AA_CODES = Dict(
+    'A' => "ALA", 'R' => "ARG", 'N' => "ASN", 'D' => "ASP",
+    'C' => "CYS", 'Q' => "GLN", 'E' => "GLU", 'G' => "GLY",
+    'H' => "HIS", 'I' => "ILE", 'L' => "LEU", 'K' => "LYS",
+    'M' => "MET", 'F' => "PHE", 'P' => "PRO", 'S' => "SER",
+    'T' => "THR", 'W' => "TRP", 'Y' => "TYR", 'V' => "VAL",
+)
+
 function collect_backbone_atoms(atoms::Vector{PDBTools.Atom})
     residues = Vector{PDBTools.Atom}[]
     i = 1
@@ -53,9 +61,9 @@ function protein_to_pdb(protein::Protein, filename, header=:auto, footer=:auto)
     index = 0
     residue_index = 0
     for chain in protein
-        backbone = chain.backbone
-        for (resnum, residue_coords) in enumerate(eachslice(backbone.coords, dims=3))
-            resname = "GLY"
+        L = length(chain)
+        for (resnum, (residue_coords, aa)) in enumerate(zip(eachslice(chain.backbone.coords, dims=3), chain.aaseq))
+            resname = THREE_LETTER_AA_CODES[aa]
             residue_index += 1
             for (name, atom_coords) in zip(["N", "CA", "C", "O"], eachcol(residue_coords))
                 index += 1
@@ -66,13 +74,31 @@ function protein_to_pdb(protein::Protein, filename, header=:auto, footer=:auto)
                     chain = chain.id,
                     resnum = resnum,
                     residue = residue_index,
-                    x = atom_coords[1],
-                    y = atom_coords[2],
-                    z = atom_coords[3],
+                    x = atom_coords[1], y = atom_coords[2], z = atom_coords[3],
                 )
                 push!(atoms, atom)
             end
         end
+
+        # reuses the magic vector from src/backbone/oxygen.jl
+        # note: the magic vector is meant to be used to calculate the O atom position,
+        # but this is basically using it to get the next N position, 
+        # so it's a hacky way to get a "random" OXT atom position
+        # not actually random -- it has the same orientation as the next-to-last residue
+        index += 1
+        last_N, last_CA, last_C, last_O = eachcol(chain.backbone.coords[:, :, L])
+        rot_matrix = get_rotation_matrix(last_CA, last_C, last_O)
+        OXT_pos = rot_matrix' \ magic_vector + last_C
+        OXT_atom = PDBTools.Atom(
+            index = index,
+            name = "OXT",
+            resname = THREE_LETTER_AA_CODES[chain.aaseq[end]],
+            chain = chain.id,
+            resnum = L,
+            residue = residue_index,
+            x = OXT_pos[1], y = OXT_pos[2], z = OXT_pos[3],
+        )
+        push!(atoms, OXT_atom)
     end
     PDBTools.writePDB(atoms, filename, header=header, footer=footer)
 end
