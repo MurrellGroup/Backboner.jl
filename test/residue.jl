4fe3b1511610ec14b488925563c4f08a954a0958
anton083 <anton.oresten42@gmail.com>
2023-11-25 21:10:09 +0100

    Update residue.jl

--- test/residue.jl
+++ test/residue.jl
@@ -2,6 +2,8 @@
     backbone = Backbone(randn(3, 4, 1))
     residue = Residue(1, backbone, 'A', Helix)
 
+    # index field get padded to digits in length of backbone, ss gets padded to 6 cause Strand is 6 chars long.
+    # Unassigned is longer but that's fine.
     @test summary(residue) == "Residue 1 ALA  Helix"
 
     io = IOBuffer()
