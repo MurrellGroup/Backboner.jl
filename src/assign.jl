e74fcf3560ed721d1fb1d4940a3618a2133939dc
anton083 <anton.oresten42@gmail.com>
2023-11-25 21:53:30 +0100

    Add non-mutating SS assign method

--- src/assign.jl
+++ src/assign.jl
@@ -1,6 +1,6 @@
-export assign_secondary_structure!
+export assign_secondary_structure!, assign_secondary_structure
 
-import AssigningSecondaryStructure as ASS
+import AssigningSecondaryStructure: assign_secondary_structure!, assign_secondary_structure
 
 """
     assign_secondary_structure!(protein)
@@ -8,10 +8,22 @@ import AssigningSecondaryStructure as ASS
 Uses a simplified version of DSSP to fill the secondary structure vector of each chain with Loop, Helix, and Strand.
 """
 function assign_secondary_structure!(protein::Protein)
-    ss_num_vectors = ASS.assign_secondary_structure([chain.backbone.coords for chain in protein])
+    ss_num_vectors = assign_secondary_structure([chain.backbone.coords for chain in protein])
     for (chain, ss_num_vector) in zip(protein, ss_num_vectors)
         ssvec = SecondaryStructure.(ss_num_vector)
         @assert length(chain.ssvec) == length(ssvec)
         chain.ssvec .= ssvec
     end
+    return protein
+end
+
+"""
+    assign_secondary_structure(protein)
+
+Returns a new protein with secondary structure assigned.
+"""
+function assign_secondary_structure(protein::Protein)
+    new_protein = deepcopy(protein)
+    assign_secondary_structure!(new_protein)
+    return new_protein
 end
--- test/assign.jl
+++ test/assign.jl
@@ -1,10 +1,18 @@
 @testset "assign.jl" begin
     
     @testset "assign_secondary_structure!" begin
-        backbone = pdb_to_protein("data/1ASS.pdb")
-        @test has_missing_ss(backbone)
-        assign_secondary_structure!(backbone)
-        @test !has_missing_ss(backbone)
+        protein = pdb_to_protein("data/1ASS.pdb")
+        @test has_missing_ss(protein)
+        assign_secondary_structure!(protein)
+        @test !has_missing_ss(protein)
+    end
+
+    @testset "assign_secondary_structure" begin
+        protein = pdb_to_protein("data/1ASS.pdb")
+        @test has_missing_ss(protein)
+        new_protein = assign_secondary_structure(protein)
+        @test has_missing_ss(protein)
+        @test !has_missing_ss(new_protein)
     end
 
 end
