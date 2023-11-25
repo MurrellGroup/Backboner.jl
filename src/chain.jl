85a6e838dbca445ae44eb255999c4abcd674b247
anton083 <anton.oresten42@gmail.com>
2023-11-25 13:58:41 +0100

    Add aaseq field

--- src/chain.jl
+++ src/chain.jl
@@ -4,34 +4,37 @@ export Chain
 """
     Chain{T}
 
-A chain has an identifier (usually a single letter) and holds the backbone atom coordinates and secondary structure of a protein chain. 
+A chain has an identifier (usually a single letter) and holds the backbone atom coordinates, amino acid sequence, and secondary structures of a protein chain. 
 """
 struct Chain{T}
     id::AbstractString
     backbone::Backbone{4,T}
-    ssvector::Vector{SecondaryStructure}
+    aaseq::Vector{Char}
+    ssvec::Vector{SecondaryStructure}
 
-    function Chain(id::AbstractString, backbone::Backbone{4,T}, ssvector::Vector{SecondaryStructure}) where T
-        @assert length(backbone) == length(ssvector) "backbone and ssvector must have the same length"
-        return new{T}(id, backbone, ssvector)
+    function Chain(
+        id::AbstractString,
+        backbone::Backbone{N,T};
+        aaseq::Vector{Char}= fill('G', length(backbone)),
+        ssvec::Union{Vector{SecondaryStructure}, Vector{<:Integer}} = fill(Unassigned, length(backbone)),
+    ) where {N,T}
+        @assert N == 3 || N == 4 "backbone must have 3 or 4 atoms per residue"
+        N == 3 && (backbone = add_oxygens(backbone))
+
+        @assert length(backbone) == length(aaseq) == length(ssvec) "backbone, aaseq, and ssvec must have the same length"
+        ssvec isa Vector{<:Integer} && (ssvec = SecondaryStructure.(ssvec))
+
+        return new{T}(id, backbone, aaseq, ssvec)
     end
 
-    function Chain(id::AbstractString, backbone::Backbone{4,T}) where T
-        return Chain(id, backbone, fill(Unassigned, length(backbone)))
-    end
-
-    function Chain(id::AbstractString, backbone::Backbone{3})
-        return Chain(id, add_oxygens(backbone))
-    end
-
-    Chain(backbone::Backbone) = Chain("", backbone) 
+    Chain(backbone::Backbone; kwargs...) = Chain("_", backbone; kwargs...) 
 end
 
-@inline Base.:(==)(chain1::Chain, chain2::Chain) = chain1.id == chain2.id && chain1.backbone == chain2.backbone && (all(==(Unassigned), chain1.ssvector) || all(==(Unassigned), chain2.ssvector) || chain1.ssvector == chain2.ssvector)
+@inline Base.:(==)(chain1::Chain, chain2::Chain) = chain1.id == chain2.id && chain1.backbone == chain2.backbone && chain1.ssvec == chain2.ssvec
 @inline Base.length(chain::Chain) = length(chain.backbone)
 @inline Base.size(chain::Chain) = (length(chain),)
 
 Base.summary(chain::Chain) = "Chain $(chain.id) with $(length(chain)) residue$(length(chain) == 1 ? "" : "s")"
 Base.show(io::IO, chain::Chain) = print(io, summary(chain))
 
-has_missing_ss(chain::Chain) = has_missing_ss(chain.ssvector)
+has_missing_ss(chain::Chain) = has_missing_ss(chain.ssvec)
