9d59fc51ce7e338d14df00b3c7187b5362e02981
anton083 <anton.oresten42@gmail.com>
2023-11-26 20:03:34 +0100

    Update residue.jl

--- test/residue.jl
+++ test/residue.jl
@@ -1,10 +1,10 @@
 @testset "residue.jl" begin
-    backbone = Backbone(randn(3, 4, 1))
+    backbone = Backbone(randn(3, 4, 100))
     residue = Residue(1, backbone, 'A', 'H')
     @test backbone_atom_coords(residue) == backbone[1]
 
     # index field get padded to digits in length of backbone
-    @test summary(residue) == "Residue 1 ALA H"
+    @test summary(residue) == "Residue   1 ALA H"
 
     io = IOBuffer()
     show(io, residue)
