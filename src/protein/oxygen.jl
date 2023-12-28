345ba5efe31a40d41c97e637f8ae48107a2675be
anton083 <anton.oresten42@gmail.com>
2023-12-28 21:08:31 +0100

    Update oxygen.jl

--- src/protein/oxygen.jl
+++ src/protein/oxygen.jl
@@ -39,23 +39,15 @@ function get_last_oxygen(
     return O_pos
 end
 
-"""
-    oxygen_coords(chain::Chain)
-
-Add oxygen atoms to the backbone of a protein, turning the coordinate array from size 3x3xL to 3x4xL-1,
-where L is the length of the backbone.
-"""
 function oxygen_coords(
-    chain::Chain,
+    backbone::Backbone,
 )
-    backbone = chain.backbone
     T = eltype(eltype(backbone))
-    L = length(chain)
+    L = length(backbone) ÷ 3
 
-    CAs = eachcol(alphacarbon_coords(chain))
-    Cs = eachcol(carbonyl_coords(chain))
-    next_Ns = eachcol(@view(nitrogen_coords(chain)[:, 2:end]))
-
+    CAs = alphacarbons(backbone)
+    Cs = carbonyls(backbone)
+    next_Ns = @view nitrogens(backbone)[2:end]
     oxygen_coords = zeros(T, 3, L)
     for (i, (CA, C, next_N)) in enumerate(zip(CAs, Cs, next_Ns))
         oxygen_coords[:, i] = estimate_oxygen_position(CA, C, next_N)
@@ -64,5 +56,32 @@ function oxygen_coords(
 
     return oxygen_coords
 end
+
+"""
+    oxygen_coords(chain::Chain)
+
+Add oxygen atoms to the backbone of a protein, turning the coordinate array from size 3x3xL to 3x4xL-1,
+where L is the length of the backbone.
+
+# Example
+```jldoctest
+julia> protein = readpdb("test/data/1ZAK.pdb")
+2-element Vector{Chain}:
+ Chain A with 220 residues
+ Chain B with 220 residues
+
+julia> oxygen_coords(protein["A"]) # returns the estimated position of oxygen atoms in chain A (~0.05 Å mean deviation)
+3×220 Matrix{Float32}:
+ 22.6697  25.1719  24.7761  25.8559  …  24.7911   22.7649   22.6578   21.24
+ 15.7257  13.505   13.5151  11.478      15.0888   12.2361   15.8825   14.2933
+ 21.4295  19.5663  22.8638  25.3283      7.95346   4.81901   3.24164  -0.742424        
+```
+
+!!! note
+    The `oxygen_coords` function finds the oxygen atoms to the backbone using idealized geometry, and oxygens atom will on average deviate [0.05 Å](https://github.com/MurrellGroup/Backboner.jl/blob/main/test/protein/oxygen.jl) from original PDB positions.
+    Moreover, the last oxygen atom is essentially given a random (although deterministic) orientation, as that information is lost when the backbone is reduced to 3 atoms, and there's no next nitrogen atom to compare with.
+"""
+oxygen_coords(chain::Chain) = oxygen_coords(chain.backbone)
+
 
 NCaCO_coords(chain::Chain) = cat(reshape(chain.backbone.coords, 3, 3, :), reshape(oxygen_coords(chain), 3, 1, :), dims=2)
