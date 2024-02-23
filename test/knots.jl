d6ab9b7d855e06eb0788e745293bcecab71d83fd
anton083 <anton.oresten42@gmail.com>
2024-02-23 01:51:09 +0100

    Add more tests

--- test/knots.jl
+++ test/knots.jl
@@ -11,4 +11,8 @@
     @test is_knotted(backbone)
     @test !is_knotted(backbone[1:end-1])
 
+    @test is_knotted(Protein.readpdb("data/1YVE.pdb")["I"].backbone)
+    @test !is_knotted(Protein.readpdb("data/1ASS.pdb")["A"].backbone)
+    @test !is_knotted(Protein.readpdb("data/1ZAK.pdb")["A"].backbone)
+
 end
