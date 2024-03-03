d13531cd9c074d5d11cf0a42871a463efadb486e
anton083 <anton.oresten42@gmail.com>
2024-03-04 00:37:16 +0100

    Remove dihedral test

--- test/ext/ZygoteIdealizationExt.jl
+++ test/ext/ZygoteIdealizationExt.jl
@@ -7,7 +7,6 @@ import Zygote
         idealized = idealize(backbone, Float32[1.46, 1.52, 1.33], Float32[1.94, 2.04, 2.13])
         @test all(isapprox.(ChainedBonds(idealized).lengths, [ideal for (_, ideal) in zip(1:length(backbone)-1, Iterators.cycle(Float32[1.46, 1.52, 1.33]))], atol=1e-3))
         @test all(isapprox.(ChainedBonds(idealized).angles, [ideal for (_, ideal) in zip(1:length(backbone)-2, Iterators.cycle(Float32[1.94, 2.04, 2.13]))], atol=1e-3))
-        @test count(<(5), abs.(ChainedBonds(backbone).dihedrals .- ChainedBonds(idealized).dihedrals))
 
         @test backbone == idealize(backbone, Float32[0], Float32[0], mask_tolerance=0.5)
     end
